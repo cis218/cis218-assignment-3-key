9cd2bee4aca7f0af5b7ae7b48e14d7d8321eaa32
David Barnes <dbarnes@kvcc.edu>
2022-05-28 15:55:29 -0400

    Add Restaurant and Review models, migrations, and admin.

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+# Rating choices for the Review model.
+RATING_CHOICES = [
+    (5, "5-Star"),
+    (4, "4-Star"),
+    (3, "3-Star"),
+    (2, "2-Star"),
+    (1, "1-Star"),
+]
+
+
+class Restaurant(models.Model):
+    """Restaurant Model"""
+
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """String"""
+        return self.name
+
+    def get_absolute_url(self):
+        """Get absolute URL"""
+        return reverse("restaurant_detail", kwargs={"pk": self.pk})
+
+
+class Review(models.Model):
+    """Review Model"""
+
+    restaurant = models.ForeignKey(
+        Restaurant,
+        on_delete=models.CASCADE,
+        related_name="reviews",
+    )
+    author = models.ForeignKey(
+        "auth.User",
+        on_delete=models.CASCADE,
+        related_name="reviews",
+    )
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    body = models.TextField()
+    rating = models.IntegerField(choices=RATING_CHOICES)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """String"""
+        return f"{self.restaurant.name} - {self.name} - {self.rating}"
+
+    def get_absolute_url(self):
+        """Get absolute URL"""
+        return reverse("review_detail", kwargs={"pk": self.pk})
