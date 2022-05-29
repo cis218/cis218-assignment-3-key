f085f5efebd293ab94be440f398c5352e984b1d0
David Barnes <dbarnes@kvcc.edu>
2022-05-29 19:47:09 -0400

    Add Login, Logout, and Sign Up Functionality. Add header to base.html to include better nav and user auth information. Add another user to db to test sign up functionality.

--- django_project/settings.py
+++ django_project/settings.py
@@ -36,13 +36,16 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "whitenoise.runserver_nostatic",
     "django.contrib.staticfiles",
     "restaurant.apps.RestaurantConfig",
+    "accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -117,8 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Where to send the user after logging in
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "login"
