30f3e4b63eee8a8be0ff0bd598b4256b49482753
David Barnes <dbarnes@kvcc.edu>
2022-05-28 20:34:13 -0400

    Add Restaurant ListView and DetailView with detail view including a list of reviews for the restaurant. Add one template tag as a sample as to what can be done.

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from .models import Restaurant, Review
+
+
+class RestaurantListView(ListView):
+    """List of Restaurants"""
+
+    model = Restaurant
+    template_name = "home.html"
+
+
+class RestaurantDetailView(DetailView):
+    """Detail of Restaurant"""
+
+    model = Restaurant
+    template_name = "restaurant_detail.html"
+    context_object_name = "restaurant"
