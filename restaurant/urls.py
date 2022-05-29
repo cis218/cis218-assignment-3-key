769180725313bf8405934d9028c241fb7ee8f425
David Barnes <dbarnes@kvcc.edu>
2022-05-29 14:43:14 -0400

    Add Detail, Create, Update, Delete views for Reviews. Update templates and partial templates to work better together.

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,8 +1,35 @@
 from django.urls import path
 
-from .views import RestaurantListView, RestaurantDetailView
+from .views import (
+    RestaurantListView,
+    RestaurantDetailView,
+    ReviewCreateView,
+    ReviewDetailView,
+    ReviewDeleteView,
+    ReviewUpdateView,
+)
 
 urlpatterns = [
+    path(
+        "restaurant/<int:restaurant_pk>/review/new/",
+        ReviewCreateView.as_view(),
+        name="review_create",
+    ),
+    path(
+        "review/<int:pk>/detail/",
+        ReviewDetailView.as_view(),
+        name="review_detail",
+    ),
+    path(
+        "review/<int:pk>/edit/",
+        ReviewUpdateView.as_view(),
+        name="review_edit",
+    ),
+    path(
+        "review/<int:pk>/delete/",
+        ReviewDeleteView.as_view(),
+        name="review_delete",
+    ),
     path(
         "restaurant/<int:pk>/",
         RestaurantDetailView.as_view(),
